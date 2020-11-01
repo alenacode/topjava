1194f4ccb6c866d1d45b9c2b26f4473179c1c46d
Alena Korol <alenacode@gmail.com>
2020-11-02 01:53:32 +1000

    5 START

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,8 +9,8 @@ INSERT INTO users (name, email, password) VALUES
     ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
-    ('ROLE_USER', 100000),
-    ('ROLE_ADMIN', 100001);
+    ('USER', 100000),
+    ('ADMIN', 100001);
 
 INSERT INTO meals (user_id, date_time, description, calories) VALUES
     (100000, TIMESTAMP '2020-01-31 20:00:00', 'description_user', 300),
